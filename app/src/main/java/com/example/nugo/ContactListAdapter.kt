63233b6f50f81a0492b14c4be0c73418d714f2be
min <nothaveacatz@gmail.com>
2024-07-23 12:09:35 +0900

    메인 액티비티에 플래그먼트 완료, 플래그먼트 1(연락처 목록) 리사이클러뷰 완료. 레이아웃 완료, 연락처 목록 전체 레이아웃 진행중

---
+++ app/src/main/java/com/example/nugo/ContactListAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.nugo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nugo.databinding.ActivityContactListItemBinding
+import com.example.nugo.databinding.FragmentContactListBinding
+
+class ContactListAdapter(
+    private val mItems:List<ContactListItem>,
+    private val itemClickListener: (ContactListItem, Int) -> Unit
+    ) : RecyclerView.Adapter<ContactListAdapter.Holder>() {
+
+        inner class Holder(private val binding: ActivityContactListItemBinding): RecyclerView.ViewHolder(binding.root){
+            fun bind(item: ContactListItem){
+                binding.tvName.text = item.tvName
+        }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ActivityContactListItemBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(mItems[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return mItems.size
+
+    }
+
+
+}
