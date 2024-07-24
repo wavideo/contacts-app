c37de1297adebb7168dab4a0d558b6f06850702c
JeonEHye <grace9189@gmail.com>
2024-07-24 16:45:39 +0900

    Refactor/ CustomDialog.kt./ 성희튜터님이 DialogFragment 추천해주셔서 내용 추가했는데 연결은 안됨. 중요한 내용없어서 참고해서 수정하고 버릴거 다 버려도 상관없음.

--- app/src/main/java/com/example/nugo/CustomDialog.kt
+++ app/src/main/java/com/example/nugo/CustomDialog.kt
@@ -1,8 +1,87 @@
-//package com.example.nugo
-//
-//import android.app.Dialog
-//import android.content.Context
-//
+package com.example.nugo
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+
+
+// A안(DialogFragment 사용하기, 황성희튜터님이 dialog보다는 dialgfragment가 좋을것같다고 추천하심.)
+// androidx.fragment.app.DialogFragment 이거랑 DialogFragment 둘중에 뭐를 사용 해야하는지 모르겠음..
+class CustomDialog : DialogFragment(){
+    // Dialog의 host에게 Event를 전달하기 위한 리스너
+    private  lateinit var listener: StopDialogListener
+
+    //interface를 구현, 각 메서드는 Dialog를 파라미터로 전달
+
+    interface StopDialogListener{
+        fun onDialogPositiveClick(dialog: DialogFragment)
+        fun onDialogNegativeClick(dialog: DialogFragment)
+    }
+
+    // 스티커 수정완료 버튼
+    private  lateinit var  positiveButton: Button
+    // 스티커 나가기 버튼(오른쪽 상단위에 X모양 button일거라고 생각하고 button 넣음)
+    private lateinit var  negativeButton: Button
+
+
+    // Click Event 전달받기 위해서 필요 위에 inerface부터 tectview, button 까지 연관된 것
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        try {
+            // 리스너 인스턴스화하여 host에게 event를 보냄
+            listener = context as StopDialogListener
+        } catch (e: ClassCastException){
+            // interface가 구현되지 않았으면 하는 excetion thorw
+            throw ClassCastException(
+                (context.toString())
+            )
+
+        }
+    }
+
+    //Custom Dialog 실행 코드
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //false로 설정하면 화면 밖 혹은 뒤로가기 버튼시 다이얼로그라 dismiss되지 않음.
+        isCancelable = true
+    }
+
+//    private lateinit var binding: CustomDialogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater?,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+//        binding = CustomDialogBinding.inflate(inflater,container,false)
+
+        val view = inflater.inflate(R.layout.dialog_sticker_detail)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        positiveButton.setOnClickListener{
+            listener.onDialogPositiveClick(this)
+        }
+        negativeButton.setOnClickListener {
+            listener.onDialogNegativeClick(this)
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
+
+
+// B안(Dialog, 아래 참고용 자료)
 //class CustomDialog(context: Context): Dialog(context) {
 //    private lateinit var itemClickListener: ItemClickListener
 //    private lateinit var binding: DialogCustomBinding
