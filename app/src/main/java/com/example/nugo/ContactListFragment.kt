5907f0f4452a417b89c10a997aab70958a427afb
min <catdosub@gmail.com>
2024-07-25 02:00:35 +0900

    자신의 프로필 이미지 변경 완료, viewpager2는 적용하다가 실패 24.07.25 오전 2시

--- app/src/main/java/com/example/nugo/ContactListFragment.kt
+++ app/src/main/java/com/example/nugo/ContactListFragment.kt
@@ -7,35 +7,23 @@ package com.example.nugo
 1. 데이터 클래스 ContactData를 통해 연락처 정보를 읽고 씁니다
 2. 연락처 상세정보 (ContactDetailFragment)로 선택한 연락처를 넘겨줍니다*/
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract.Contacts
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nugo.databinding.FragmentContactListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContactListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private val binding by lazy { FragmentContactListBinding.inflate(layoutInflater) }
-    private lateinit var adapter : ContactListAdapter
-    //    private var _binding: FragmentContactListBinding? = null
-//    private val binding get() = _binding!!
-    private val PICK_IMAGE_REQUEST = 1
+    private lateinit var adapter: ContactListAdapter
+    private val PICK_IMAGE_REQUEST = 1 // 추가된 부분: 이미지 선택 요청 코드
     private var param1: String? = null
     private var param2: String? = null
 
@@ -51,21 +39,19 @@ class ContactListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        _binding = FragmentContactListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
         adapter = ContactListAdapter(ContactManager.Contacts) // adapter 초기화
         binding.recycleListView.adapter = adapter
-        binding.recycleListView.layoutManager = LinearLayoutManager(this.requireContext())
+        binding.recycleListView.layoutManager = LinearLayoutManager(requireContext())
+
+        binding.ivMyInfo.setOnClickListener {  // 추가된 부분: 이미지 선택 버튼 클릭 리스너
+            openImageChooser()  // 추가된 부분: 이미지 선택 메소드 호출
+        }
 
         adapter.itemClick = object : ContactListAdapter.ItemClick {
             override fun onClick(view: View, position: Int) {
@@ -82,9 +68,21 @@ class ContactListFragment : Fragment() {
         binding.addFriend.setOnClickListener {
             val intent_addFriend = Intent(requireContext(), AddFriendActivity::class.java)
             startActivity(intent_addFriend)
-//            adapter.notifyDataSetChanged() // 전부 다 수정(대규모 데이터 처리 비추)
         }
+    }
 
+    private fun openImageChooser() {  // 멤버 함수로 정의
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            val imageUri = data.data
+            binding.ivMyInfo.setImageURI(imageUri)
+        }
     }
 
     override fun onResume() {
@@ -92,22 +90,11 @@ class ContactListFragment : Fragment() {
         adapter.updateData(ContactManager.Contacts)
     }
 
-
-
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ContactListFragment().apply {
-                var arguments = Bundle().apply {
+                arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
